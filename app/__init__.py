1eff9cdb5beba9ad12919c77be72b682fb73e798
Joshua T. Minor <imaginarycartography@mail.com>
2025-02-06 13:29:54 -0800

    cleaning up tests and docs
    small changes

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,13 @@ Create and configure the Flask application.
 from flask import Flask
 from flask_cors import CORS
 
-from app.main import Routes
+from app.main import RestAPIRoutes
 
 def create_app():
     """Create and configure the Flask application."""
     app = Flask(__name__)
     CORS(app)
 
-    app.register_blueprint(Routes.main_bp)
+    app.register_blueprint(RestAPIRoutes.main_bp)
 
     return app
